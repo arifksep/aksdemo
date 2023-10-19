54d2c4c3576e2d0fb111e1b0ad18fc9778a4070b
Mohammed Arif Khan <mohkh@microsoft.com>
2023-10-19 14:45:57 +0530

    added web api to be hosted in linux node pool in aks

---
+++ WebAppService/WebAppService/Controllers/DayOfWeekController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DayOfWeekController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return DateTime.Now.DayOfWeek.ToString();
+        }
+    }
+}
